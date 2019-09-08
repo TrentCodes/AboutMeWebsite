d72e4a497b4b66659c2980ce004f52419e906849
TrentCodes <talon011@fiu.edu>
2019-09-08 16:27:03 -0400

    fixed path in boot.rb

--- aboutmewebsite/config/boot.rb
+++ aboutmewebsite/config/boot.rb
@@ -1,4 +1,4 @@
-ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('C:/Users/Trent/Documents/SeniorProject/AboutMeWebsite/Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
